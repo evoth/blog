1832ee6e0c9ffe9c4ffbdf193f9988bdc365d5b3
Ethan Voth <ethanvoth1@gmail.com>
2023-08-29 02:44:47 -0400

    SVG thumbnail animations only on hover

---
+++ static/js/svg-thumbs.js
@@ -0,0 +1,41 @@
+/**
+ * Unpauses SVG animation if not already animating; continues animation otherwise
+ */
+function cardEnter(svg) {
+    // Increments counter to cancel any pausing queued previously
+    svg.dataset.counter = String(Number(svg.dataset.counter ?? 0) + 1);
+    // Unpauses animations
+    svg.unpauseAnimations();
+}
+/**
+ * Pauses SVG animation, waiting until looping point if necessary
+ */
+function cardLeave(svg) {
+    const counter = svg.dataset.counter ?? "0";
+    // If loopSeconds is not specified, pause immediately
+    if (!svg.dataset.loopSeconds) {
+        svg.pauseAnimations();
+        return;
+    }
+    // If loopSeconds is specified, wait until multiple of loopSeconds to pause.
+    const loopSeconds = Number(svg.dataset.loopSeconds);
+    const delay = loopSeconds - (svg.getCurrentTime() % loopSeconds);
+    console.log(loopSeconds, svg.getCurrentTime(), svg.getCurrentTime() % loopSeconds, delay);
+    // Pauses after delay, unless counter has changed (another mouseenter event)
+    setTimeout(function () {
+        if (svg.dataset.counter != counter)
+            return;
+        svg.pauseAnimations();
+        svg.setCurrentTime(0);
+    }, delay * 1000);
+}
+// Adds event listeners to cards that have svg thumbnails
+const svgs = document.getElementsByClassName("card-svg-thumb");
+for (const svg of svgs) {
+    svg.pauseAnimations();
+    svg.setCurrentTime(0);
+    const card = svg.closest(".card");
+    card.addEventListener("mouseenter", () => cardEnter(svg));
+    card.addEventListener("mouseleave", () => cardLeave(svg));
+}
+export {};
